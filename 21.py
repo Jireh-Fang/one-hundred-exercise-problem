f7d17e964bd31ad1a2721b0d6a7ee693c997ace2
Jireh-Fang <52043755+Jireh-Fang@users.noreply.github.com>
2019-10-20 15:36:34 +0800

    #取数字中从右端开始的4~7位

---
+++ 21.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 20 15:31:06 2019
+
+@author: Administrator
+"""
+
+#取一个整数a从右端开始的4~7位，如431421467
+a = 431421467
+str_a = str(a)
+b = str_a[-7:-3]
+print(b)
