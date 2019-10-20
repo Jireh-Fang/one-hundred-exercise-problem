af7129b48ac432300c965a88ceb066f6d3507988
Jireh-Fang <52043755+Jireh-Fang@users.noreply.github.com>
2019-10-20 15:30:50 +0800

    20_输入数字，判断输出

---
+++ 20.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 20 15:24:24 2019
+
+@author: Administrator
+"""
+
+#输入数字，如果平方运算后小于50则退出
+num = int(input('请输入一个数字：'))
+if num**2 < 50:
+    print('退出')
+else:
+    print('待定')
