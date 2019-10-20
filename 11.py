4b252a13b896d02809fbacfcb38a7a1825a6ecf2
Jireh-Fang <52043755+Jireh-Fang@users.noreply.github.com>
2019-10-20 13:55:09 +0800

    11_打印图形3

---
+++ 11.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 20 13:48:51 2019
+
+@author: Administrator
+"""
+
+#打印图形3
+num1 = 5
+num2 = 2
+print('*'*num1)
+print('*'+' '*2+'*')
+print('*'+' '+'*')
+print('*'*num2)
+print('*')
