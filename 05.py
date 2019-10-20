77f5e2c4e6ec61b5a78e5c95c2fe01e0f3016039
Jireh-Fang <52043755+Jireh-Fang@users.noreply.github.com>
2019-10-20 12:57:37 +0800

    05_输出字符串的长度

---
+++ 05.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 20 12:55:26 2019
+
+@author: Administrator
+"""
+
+#计算'Welcome to Crossin的编程教室!'的长度
+a = 'Welcome to Crossin的编程教室!'
+length = len(a)
+print(length)
