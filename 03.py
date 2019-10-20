31e25edf8aee36bd0158aa7a780a92365b3caf25
Jireh-Fang <52043755+Jireh-Fang@users.noreply.github.com>
2019-10-20 12:53:15 +0800

    反序输出

---
+++ 03.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 20 12:46:10 2019
+
+@author: Administrator
+"""
+
+#按相反的顺序输出列表['one','two','three']的值
+a = ['one','two','three']
+b = a[::-1]
+for i in b:
+    print(i)
