6795b95b356bacb369a4acdbbcb77f0bcd3f1a2b
Jireh-Fang <52043755+Jireh-Fang@users.noreply.github.com>
2019-10-28 11:18:50 +0800

    36_判断一个数是奇数还是偶数

---
+++ 36.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 28 11:15:05 2019
+
+@author: Administrator
+"""
+
+#判断输入数字为奇数还是偶数
+num = int(input('请输入一个数字：'))
+if num % 2 == 0:
+    print('该数是偶数')
+else:
+    print('该数是奇数')
