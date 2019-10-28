1257682b83bb3d9b41ba92d051fec6d475b4a554
Jireh-Fang <52043755+Jireh-Fang@users.noreply.github.com>
2019-10-28 11:25:22 +0800

    37_矩阵相加

---
+++ 37.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 28 11:19:16 2019
+
+@author: Administrator
+"""
+
+import numpy as np
+arr1 = np.array([[12,7,3],[4,5,6],[7,8,9]])
+arr2 = np.array([[5,8,1],[6,7,3],[4,5,9]])
+arr3 = arr1 + arr2
+print(arr3)
