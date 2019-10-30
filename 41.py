f1e294f74a9577bc94a45cddd953f59cd3155bc1
Jireh-Fang <52043755+Jireh-Fang@users.noreply.github.com>
2019-10-30 21:00:46 +0800

    41_用*替换字符串中的好

---
+++ 41.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 30 20:47:14 2019
+
+@author: Administrator
+"""
+
+#使用*替换字符串中的好
+str1 = '你好，我好，大家好才是真的好！'
+str2 = str1.replace("好","*")      #replace(old,new,count)
+print(str2)
