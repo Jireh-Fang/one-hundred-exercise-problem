501cf6ed1feca3ba16c6c950b46804ed310b8844
Jireh-Fang <52043755+Jireh-Fang@users.noreply.github.com>
2019-10-31 09:36:07 +0800

    46_计算列表元素之积

---
+++ 46.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 31 09:32:58 2019
+
+@author: Administrator
+"""
+
+list1 = [2,4,1,2]
+accumulate = 1
+for i in list1:
+     accumulate *= i
+print(accumulate)
