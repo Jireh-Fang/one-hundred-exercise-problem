7c4ceac6987acce19af9cdebd6456cb33c9503f2
Jireh-Fang <52043755+Jireh-Fang@users.noreply.github.com>
2019-10-31 09:41:46 +0800

    47_计算列表中最大数与最小数之差

---
+++ 47.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 31 09:36:24 2019
+
+@author: Administrator
+"""
+
+list1 = [2,4,1,2]
+list1.sort()
+difference = list1[-1] - list1[0]
+print(difference)
