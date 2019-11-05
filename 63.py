7791bc85312c08ab8c08dc568e85a0d19835e8ed
Jireh-Fang <52043755+Jireh-Fang@users.noreply.github.com>
2019-11-05 16:18:06 +0800

    63_打印菱形图形

---
+++ 63.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  5 16:13:11 2019
+
+@author: Administrator
+"""
+
+print('   *   ')
+print('  ***   ')
+print(' *****  ')
+print('*******')
+print(' *****  ')
+print('  ***   ')
+print('   *   ')
+    
